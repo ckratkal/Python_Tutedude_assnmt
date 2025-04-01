7a7ec2e844aad7a4241a56ca3f2ed1de647efac4
Chetan Kulkarni <113508777+ckratkal@users.noreply.github.com>
2025-04-01 15:18:26 +0530

    Added Task1 And Task 2 of Assignment 1

---
+++ BasicArthmatic.py
@@ -0,0 +1,8 @@
+#Calculate basic Math Operations(+,-,*,/)
+
+#Intput two number
+num1 = int(input("Enter your 1st number: "))
+num2 = int(input("Enter your 2nd number: "))
+#print with arthematic operation within statement using formated string
+print(f'Addition {num1+num2}\nSubstration {num1-num2}\nMultiplication{num1*num2}\nDivison {num1/num2}')
+
---
+++ Greeting.py
@@ -0,0 +1,9 @@
+###welcome greeting
+
+#Accept Input
+First_Name = input("Enter your first name: ")
+Last_Name = input("Enter your Second name: ")
+#Sting Concantination with space
+Full_Name = First_Name + " " + Last_Name 
+#print the string
+print(f'Hello, {Full_Name}! Welcome to python programming ')
